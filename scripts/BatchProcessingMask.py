4bacc27ef74c2d8ba5d66fff0cb568a2b424fe00
Lu_PC <ly1750020733@outlook.com>
2025-10-27 20:50:05 +0800

    对batchProcessingMark进行修改，使其能够掩膜CF与DEM。对CalculateThickness进行修改，修复了nodata干扰plt的bug与错误提取行、列的错误

--- scripts/BatchProcessingMask.py
+++ scripts/BatchProcessingMask.py
@@ -10,14 +10,18 @@ folder = Path(r"E:\code\geo_processing\database\UnifiedProject")#路径到文件
 shp_list = list(folder.glob("*.shp"))
 # 读取 shapefile
 shp_path = shp_list
+
+# 读取栅格
+tif_path_DEM = r"E:\code\geo_processing\database\DEM\ZhangDEM1.tif"
+tif_path_CF = r"E:\code\geo_processing\database\DEM\ZhangCF.tif"
+
 for shp in shp_path:
     gdf = gpd.read_file(shp)
     geoms = [mapping(geom) for geom in gdf.geometry] #单个与多个不冲突
 
-    # 读取栅格
-    tif_path = r"E:\code\geo_processing\database\zhangzhongjing1.tif"
+    #DEM
     # 打开 tif 并掩膜
-    with rasterio.open(tif_path) as src:
+    with rasterio.open(tif_path_DEM) as src:
         out_image, out_transform = mask(src, geoms, crop=True)
         out_meta = src.meta.copy()
 
@@ -30,10 +34,36 @@ for shp in shp_path:
     })
 
     shp_name = shp.stem  # 只获取文件名（不含路径、不含后缀）
-    output_folder = r"E:\code\geo_processing\database\testdata\mask"
+    output_folder = r"E:\code\geo_processing\database\testdata\mask\DEM"
+    Path(output_folder).mkdir(parents=True, exist_ok=True)# ✅ 如果文件夹不存在则自动创建
     output_filepath = f"{output_folder}/{shp_name}.tif"
 
     with rasterio.open(output_filepath, "w", **out_meta) as dest:
         dest.write(out_image)
 
-    print("掩膜完成，输出文件：", output_filepath)
+    print("DEM掩膜完成，输出文件：", output_filepath)
+
+    # cf
+    # 打开 tif 并掩膜
+    with rasterio.open(tif_path_CF) as src:
+        out_image, out_transform = mask(src, geoms, crop=True)
+        out_meta = src.meta.copy()
+
+    # 更新元数据并写出结果
+    out_meta.update({
+        "driver": "GTiff",
+        "height": out_image.shape[1],
+        "width": out_image.shape[2],
+        "transform": out_transform
+    })
+
+    shp_name = shp.stem  # 只获取文件名（不含路径、不含后缀）
+    output_folder = r"E:\code\geo_processing\database\testdata\mask\CF"
+    Path(output_folder).mkdir(parents=True, exist_ok=True)  # ✅ 如果文件夹不存在则自动创建
+    output_filepath = f"{output_folder}/{shp_name}.tif"
+
+    with rasterio.open(output_filepath, "w", **out_meta) as dest:
+        dest.write(out_image)
+
+    print("CF掩膜完成，输出文件：", output_filepath)
+
--- scripts/CalculateThickness.py
+++ scripts/CalculateThickness.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1. 打开 DEM
-tif_path = r"E:\code\geo_processing\database\testdata\0.61232.tif"
+tif_path = r"E:\code\geo_processing\database\testdata\mask\DEM\0.61232.tif"
 with rasterio.open(tif_path) as src:
     img = src.read()         # numpy 数组，形状 (bands, height, width)
     profile = src.profile    # 保存元数据（投影、分辨率等）
 
 row_median = img.shape[2] // 2
 col_median = img.shape[1] // 2
-row_values = img[0,row_median-1,:]  #0表示第一波段
+row_values = img[0,col_median-1,:]  #0表示第一波段，row_median-1是因为从0行/列开始，行的值是中间列
 # 定义一个极小阈值，例如 -3e10
 threshold = -3e10
 # 判断首尾是否为异常值
@@ -19,7 +19,7 @@ if row_values[0] < threshold:   #边缘值可能为nodata=-3.402823e+38，舍弃
 if row_values[-1] < threshold:
     row_values = row_values[:-1]  # 去掉最后一个
 
-col_values = img[0, :, col_median-1]
+col_values = img[0, :, row_median-1]
 # 判断首尾是否为异常值
 if col_values[0] < threshold:   #边缘值可能为nodata=-3.402823e+38，舍弃
     col_values = col_values[1:]  # 去掉第一个
@@ -32,21 +32,21 @@ x2 = np.arange(0, len(row_values), 1)
 y2 = row_values
 
 # 2. 打开 CF
-tif_path2 = r"E:\code\geo_processing\database\testdata\0.61232CFnodata0.tif"
+tif_path2 = r"E:\code\geo_processing\database\testdata\mask\CF\0.61232.tif"
 with rasterio.open(tif_path2) as src2:
     img2 = src2.read()         # numpy 数组，形状 (bands, height, width)
     profile2 = src2.profile    # 保存元数据（投影、分辨率等）
 
 row_median2 = img2.shape[2] // 2
 col_median2 = img2.shape[1] // 2
-row_values2 = img2[0,row_median2-1,:]  #0表示第一波段
+row_values2 = img2[0,col_median2-1,:]  #0表示第一波段
 # 判断首尾是否为异常值
 if row_values2[0] < threshold:   #边缘值可能为nodata=-3.402823e+38，舍弃
     row_values2 = row_values2[1:]  # 去掉第一个
 if row_values2[-1] < threshold:
     row_values2 = row_values2[:-1]  # 去掉最后一个
 
-col_values2 = img2[0, :, col_median2-1]
+col_values2 = img2[0, :, row_median2-1]
 # 判断首尾是否为异常值
 if col_values2[0] < threshold:   #边缘值可能为nodata=-3.402823e+38，舍弃
     col_values2 = col_values2[1:]  # 去掉第一个
@@ -58,7 +58,6 @@ x3 = np.arange(0, len(col_values2), 1)
 y3 = col_values2
 x4 = np.arange(0, len(row_values2), 1)
 y4 = row_values2
-
 
 plt.subplot(2, 2, 1)
 plt.plot(x1, y1, label='elevation', color='blue', linewidth=2)
@@ -93,4 +92,10 @@ plt.ylabel("Y axis")
 plt.legend()  # 显示图例
 plt.grid(True)  # 显示网格
 
+plt.savefig(r"E:\code\geo_processing\database\testdata\show\show.png")
 plt.show()
+
+np.savetxt(r"E:\code\geo_processing\database\testdata\show\ColCalculateThickness_xy_values.csv", np.column_stack((x1, y1)), delimiter=",", header="x,y", comments='')#去掉文件头部前面的表示去除csv的“#”
+np.savetxt(r"E:\code\geo_processing\database\testdata\show\RowCalculateThickness_xy_values.csv", np.column_stack((x2, y2)), delimiter=",", header="x,y", comments='')
+np.savetxt(r"E:\code\geo_processing\database\testdata\show\ColCF_xy_values.csv", np.column_stack((x3, y3)), delimiter=",", header="x,y", comments='')
+np.savetxt(r"E:\code\geo_processing\database\testdata\show\RowCF_xy_values.csv", np.column_stack((x4, y4)), delimiter=",", header="x,y", comments='')
